06cf3d2ce02d1b91b3001b2d01ddbdea54b69fda
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-09-02 12:46:09 +0700

    Create subfzuzing.sh
    Find subdomain to attack (red team)

---
+++ subfzuzing.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+help(){
+    echo -e "[Usage]:"
+    echo -e "\t$ ~/subdomain.sh DOMAIN"
+}
+
+getSubdomains(){
+    curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$1/passive_dns" | sort -u > tmp.txt &
+    curl -s "https://jldc.me/anubis/subdomains/$1" | cut -d '"' -f2 | cut -d '[' -f1 | cut -d ']' -f1 | grep . | sort -u >> tmp.txt &
+    curl -s "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sort | sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^ww>    curl -s "https://certspotter.com/api/v0/certs?domain=$1" | sed 's/\"//g' | sed 's/\*\.//g' | grep -w $1\$ | sort -u >> tmp.txt &
+    curl -s "https://crt.sh/?q=%.$1&output=json" | sed 's/\*\.//g' | sort -u >> tmp.txt &
+    curl -s "https://dns.bufferover.run/dns?q=.$1" | cut -d ',' -f2 | grep -o "\w.*$1" | sort -u >> tmp.txt &
+    curl -s "https://dns.bufferover.run/dns?q=.$1" |  cut -d ',' -f2 | grep -o "\w.*$1" | sort -u >> tmp.txt &
+    curl -s "https://tls.bufferover.run/dns?q=.$1" | cut -d ',' -f3 | grep -o "\w.*$1"| sort -u >> tmp.txt &
+    curl -s "https://api.hackertarget.com/hostsearch/?q=$1" | cut -d ',' -f1 | sort -u >> tmp.txt &
+    curl -s "https://rapiddns.io/subdomain/$1?full=1#result" | grep -oaEi "https?://[^\"\\'> ]+" | grep $1 | sed 's/https\?:\/\///' | cut -d "/" -f3 | sort -u >> tmp.txt &
+    curl -s "https://riddler.io/search/exportcsv?q=pld:$1" | grep -o "\w.*$1" | cut -d ',' -f6 | sort -u >> tmp.txt &
+    curl -s "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=$1" | jq '.subdomains' | cut -d '"' -f2 | cut -d '[' -f1 | cut -d ']' -f1 | grep . | sort -u >>>    curl -s "https://api.threatminer.org/v2/domain.php?q=$1&rt=5" | sort -u >> tmp.txt &
+    curl -s "https://urlscan.io/api/v1/search/?q=domain:$1" | sort -u >> tmp.txt &
+    curl -s "https://www.virustotal.com/ui/domains/$1/subdomains?limit=40" | grep '"id":' | cut -d '"' -f4 | sort -u >> tmp.txt &
+    csrftoken=$(curl -ILs https://dnsdumpster.com | grep csrftoken | cut -d " " -f2 | cut -d "=" -f2 | tr -d ";")
+    curl -s --header "Host:dnsdumpster.com" --referer https://dnsdumpster.com --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0" ->    if [[ -e $1 && -s $1 ]]; then # file exists and is not zero size
+        cat dnsdumpster.html | grep "https://api.hackertarget.com/httpheaders" | grep -o "\w.*$1" | cut -d "/" -f7 | grep '.' | sort -u >> tmp.txt
+    fi
+    cat tmp.txt | grep -iv "*" | sort -u | grep $1 | tee
+    rm -rf dnsdumpster.html
+    rm -rf tmp.txt
+}
+
+if [[ -z $1 ]]; then help; else getSubdomains $1; fi
